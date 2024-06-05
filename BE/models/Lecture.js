c3c2024a34a076914eaac0769da553f2697c6cce
YGC20 <ygchun123@gmail.com>
2024-06-06 00:21:16 +0900

    feat: Bug Fix

--- BE/models/Lecture.js
+++ BE/models/Lecture.js
@@ -18,12 +18,12 @@ class Lecture {
     }
 
     static async findroom(roomData){
-        return new Promise(async (resolve, rejects) => {
+        return new Promise(async (resolve, reject) => {
             const { build, roomNum } = roomData;
             const query = 'SELECT * FROM lecture WHERE building = ? AND lecture_room = ?';
             connection.query(query, [build, roomNum], (err, results) => {
                 if(err){
-                    return rejects(err);
+                    return reject(err);
                 }
                 resolve(results);
             });
