09bf701a0dd68934ae95d3facd6ba3966f70a116
ComAiKSY <suyun101014@gmail.com>
2024-06-10 03:28:29 +0900

    Feature
    UI UX 개선, MyPageInfo 개선

--- FE/src/pages/MyPageInfo.jsx
+++ FE/src/pages/MyPageInfo.jsx
@@ -25,38 +25,39 @@ export default function MyPageInfo() {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center p-8">
-      <h1 className="text-3xl font-bold mb-4 custom-font">{"< 기본 회원 정보 >"}</h1>
-      <div className="grid grid-cols-2 gap-0 border border-black" style={{ width: '80%', maxWidth: '600px' }}>
-        {userInfo && (
-          <>
-            <div className="flex items-center justify-center border-t-2 bg-gray-300 border border-black p-4 h-20">
-              <p style={{ fontSize: "20pt" }} className="font-bold custom-font text-left">이름</p>
-            </div>
-            <div className="flex items-center border border-t-2 border-black p-4 h-20">
-              <div className="w-full text-left pl-6 font-bold custom-font" style={{ fontSize: "16pt" }}>
-                {userInfo.name}
-              </div>
-            </div>
-            <div className="flex items-center justify-center bg-gray-300 border border-black p-4 h-20">
-              <p style={{ fontSize: "20pt" }} className="font-bold custom-font">아이디</p>
-            </div>
-            <div className="flex items-center border border-black p-4 h-20">
-              <div className="w-full text-left pl-6 font-bold custom-font" style={{ fontSize: "16pt" }}>
-                {userInfo.id}
-              </div>
-            </div>
-            <div className="flex items-center justify-center bg-gray-300 border border-black p-4 h-20">
-              <p style={{ fontSize: "20pt"}} className="font-bold custom-font">학번</p>
-            </div>
-            <div className="flex items-center border border-black p-4 h-20 ">
-              <div className="w-full text-left pl-6 font-bold custom-font " style={{ fontSize: "16pt" }}>
-                {userInfo.studentId}
-              </div>
-            </div>
-          </>
-        )}
+    <div className="flex flex-col items-center justify-center h-max p-5">
+      <div className="w-full text-left mb-10 mt-2">
+        <h1 className="text-4xl font-bold text-gray-500 custom-font">
+          나의 정보
+          <span className="text-xl ml-2 custom-font text-gray-400">가입하신 회원 정보입니다.</span>
+        </h1>
+        <hr className="border-t-2 border-gray-300 mt-6" />
+      </div>
+    
+      
+      {userInfo && (
+  <>
+    <div className="flex items-center mb-5 mt-8">
+      <p className="font-bold w-40 custom-font text-right" style={{ fontSize: "16pt", marginRight: "50px" }}>이름</p>
+      <div className="border border-black p-2 w-80 text-left pl-4 font-bold custom-font" style={{ fontSize: "15pt" }}>
+        {userInfo.name}
       </div>
     </div>
+    <div className="flex items-center mb-5">
+      <p className="font-bold w-40 custom-font text-right" style={{ fontSize: "16pt", marginRight: "50px" }}>아이디</p>
+      <div className="border border-black p-2 w-80 text-left pl-4 font-bold custom-font" style={{ fontSize: "15pt" }}>
+        {userInfo.id}
+      </div>
+    </div>
+    <div className="flex items-center mb-5">
+      <p className="font-bold w-40 custom-font text-right" style={{ fontSize: "16pt", marginRight: "50px" }}>학번</p>
+      <div className="border border-black p-2 w-80 text-left pl-4 font-bold custom-font" style={{ fontSize: "15pt" }}>
+        {userInfo.studentId}
+      </div>
+    </div>
+    <hr className="border-t-2 border-gray-300 w-full mb-14 mt-16" />
+  </>
+)}
+      </div>
   );
 }
